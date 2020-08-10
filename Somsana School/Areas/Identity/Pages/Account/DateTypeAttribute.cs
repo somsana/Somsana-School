3343345b830622b5e86db122b518922f5db68dee
Somsana Ninthavong <ac100551@avcol.school.nz>
2020-08-11 10:10:54 +1200

    9

---
+++ Somsana School/Areas/Identity/Pages/Account/DateTypeAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Somsana_School.Areas.Identity.Pages.Account
+{
+    internal class DateTypeAttribute : Attribute
+    {
+        private DataType text;
+
+        public DateTypeAttribute(DataType text)
+        {
+            this.text = text;
+        }
+    }
+}
--- Somsana School/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Somsana School/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -46,6 +46,16 @@ namespace Somsana_School.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
+            [DateType(DataType.Text)]
+            [Display(Name = "First Name")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [DateType(DataType.Text)]
+            [Display(Name = "Last Name")]
+            public string LastName { get; set; }
+
+            [Required]
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
@@ -74,7 +84,7 @@ namespace Somsana_School.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new IdentityUser { UserName = Input.Email, Email = Input.Email };
+                var user = new IdentityUser { UserName = Input.Email, Email = Input.Email,FirstName = Input.FirstName,LastName=Input.LastName };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
